43369c9e0bb9d62fa3f6483940ec470e15a14274
TerrorBite <terrorbite@lethargiclion.net>
2012-10-08 01:08:22 +1000

    Initial commit with BukkitPlugin archetype defaults

---
+++ src/main/java/net/lethargiclion/LethargicPerms/LethargicPerms.java
@@ -0,0 +1,46 @@
+package net.lethargiclion.LethargicPerms;
+
+/*
+    This file is part of LethargicPerms
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LethargicPerms extends JavaPlugin {
+
+	//ClassListeners
+	private final LethargicPermsCommandExecutor commandExecutor = new LethargicPermsCommandExecutor(this);
+	private final LethargicPermsEventListener eventListener = new LethargicPermsEventListener(this);
+	//ClassListeners
+
+	public void onDisable() {
+		// add any code you want to be executed when your plugin is disabled
+	}
+
+	public void onEnable() { 
+
+		PluginManager pm = this.getServer().getPluginManager();
+
+		getCommand("command").setExecutor(commandExecutor);
+
+		// you can register multiple classes to handle events if you want
+		// just call pm.registerEvents() on an instance of each class
+		pm.registerEvents(eventListener, this);
+
+		// do any other initialisation you need here...
+	}
+}
---
+++ src/main/java/net/lethargiclion/LethargicPerms/LethargicPermsCommandExecutor.java
@@ -0,0 +1,44 @@
+package net.lethargiclion.LethargicPerms;
+
+/*
+    This file is part of LethargicPerms
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class LethargicPermsCommandExecutor implements CommandExecutor {
+
+    private LethargicPerms plugin;
+
+    public LethargicPermsCommandExecutor(LethargicPerms plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        plugin.getLogger().info("onCommand Reached in LethargicPerms");
+
+        if (command.getName().equalsIgnoreCase("command")) {
+	    plugin.getLogger().info("command used");
+            //do something
+            return true;
+        }
+        return false;
+    }
+}
